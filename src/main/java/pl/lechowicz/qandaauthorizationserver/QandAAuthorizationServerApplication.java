f69995f73cac40e12562d558191058c47143698d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2024-02-28 20:58:00 +0100

    Finish Code Grant Authorization chapter

---
+++ src/main/java/pl/lechowicz/qandaauthorizationserver/QandAAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package pl.lechowicz.qandaauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QandAAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QandAAuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lechowicz/qandaauthorizationserver/QandAAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lechowicz.qandaauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QandAAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
